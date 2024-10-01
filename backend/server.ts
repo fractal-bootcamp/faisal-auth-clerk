eab8917ed061ada779065be14a2777dec1a06c12
Faisal <faisal@owimer.co>
2024-10-01 11:53:06 -0400

    inital clerk setup for frontend & backend

--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,30 @@
 import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import { clerkMiddleware, clerkClient, getAuth, requireAuth } from "@clerk/express";
+
+dotenv.config()
 
 const app = express()
 const PORT = 3000
 
+app.use(cors({ origin: "http://localhost:5174" }))
 app.use(express())
 app.use(express.json())
+app.use(clerkMiddleware())
 
 app.get("/", (_req, res) => {
     res.send("I am alive")
+})
+
+app.get("/protected", requireAuth({ signInUrl: "/sign-in" }), async (req, res) => {
+    const { userId } = getAuth(req)
+    const user = await clerkClient.users.getUser(userId)
+})
+
+app.get("/sign-in", (req, res) => {
+
+    res.render("sign-in")
 })
 
 app.listen(PORT, () => {
---
+++ frontend/clerk-react/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Login from './components/Login'
+
+const App = () => {
+
+  return (
+    <div>
+      I am alive
+      <Login />
+    </div>
+  )
+}
+
+export default App
